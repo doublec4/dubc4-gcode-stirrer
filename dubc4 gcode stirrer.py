0aa7500b0ccaaed4b052e0db4776bf515d2ad2a4
ES-Alexander <sandman.esalexander@gmail.com>
2021-08-07 14:49:35 +1000

    Add compatibility mode, optional inputs
    Compatibility disables using M808 command

--- dubc4 gcode stirrer.py
+++ dubc4 gcode stirrer.py
@@ -2,19 +2,22 @@
 import math
 
 class StirGCodeGenerator:
-    def __init__(self, xMax, yMax, zMax, zFinal, stirDiameter, stirSpeed, stirTime, stirHeight, travelSpeed=2400):
+    def __init__(self, printerDims, zFinal, stirDiameter, stirSpeed, stirTime, stirHeight,
+                 travelSpeed=2400, compatibility=False):
         """ Creates a GCode generator for stirring a set amount of time.
         
-        'xMax' printer length [mm]
-        'yMax' printer width [mm]
-        'zMax' printer height [mm]
-        'zFinal' stirrer height at the end of stirring [mm], must be less than 'zMax'
-        'stirDiameter' diameter of the circle to trace while stirring [mm], must be less than 'xMax' and 'yMax'
+        'printerDims' is a tuple of printer dimensions (length (x), width (y), height (z)) [mm]
+        'zFinal' stirrer height at the end of stirring [mm], must be less than printer height
+        'stirDiameter' diameter of the circle to trace while stirring [mm],
+            Must be less than printer length and width.
         'stirSpeed' speed at which to stir [mm/sec]
         'stirTime' duration of stirring [mins]
         'travelSpeed' speed of travel moves [mm/sec]
+        'compatibility' boolean for whether to support old firmwares (if True disallows M808 repeat)
+            Defaults to False (M808 allowed).
         
         """
+        xMax, yMax, zMax = printerDims
         self.center = [round(float(xMax) / 2, 2), round(float(yMax) / 2, 2), round(float(zMax) / 2, 2)]
         self.zFinal = round(float(zFinal))
         self.stirRadius = round(float(stirDiameter) / 2, 2)
@@ -22,6 +25,7 @@ class StirGCodeGenerator:
         self.stirSpeed = round(float(stirSpeed) * 60, 2)
         self.stirHeight = round(float(stirHeight), 2)
         self.travelSpeed = round(float(travelSpeed))
+        self.compatibility = compatiblity
 
     def generate(self, filename):
         """ Generates gcode and writes to 'filename'.
@@ -57,6 +61,13 @@ class StirGCodeGenerator:
         )
     
     def generate_stirring(self, xOffset, yOffset):
+        if self.compatibility:
+            return (
+                (";Stirring",
+                 *(f"G2 X{xOffset} Y{yOffset} I{self.stirRadius} J0 F{self.stirSpeed}" 
+                   for _ in range(self.loops)))
+            )
+        
         return (
             (";Start Loop",
              f"M808 L{self.loops}"),
@@ -94,7 +105,10 @@ stirSpeed    = input("Stirring speed (mm/sec): ")
 stirTime     = input("Stirring duration (min): ")
 stirHeight   = input("Stirrer height (mm): ")
 zFinal       = input("Final stirrer height (mm): ")
+travelSpeed  = input("Travel speed (mm/sec - default 2400): ") or 2400
+disable_M808 = input("Compatiblity mode (disable M808) [y/N]?: ").lower() == 'y'
 fileName     = input("Enter filename: ")
 
-g = StirGCodeGenerator(xMax, yMax, zMax, zFinal, stirDiameter, stirSpeed, stirTime, stirHeight)
+g = StirGCodeGenerator((xMax, yMax, zMax), zFinal, stirDiameter, stirSpeed, stirTime, stirHeight,
+                       travelSpeed, disable_M808)
 g.generate(fileName)
